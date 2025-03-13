8a69d930db0f4b9bd9321687f98e699657d6560b
mohamed1383 <103193197+mohamed1383@users.noreply.github.com>
2025-03-14 00:37:01 +0330

    Update chart.jsx

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -6,7 +6,6 @@ import {
   YAxis,
   Tooltip,
   CartesianGrid,
-  Legend,
   ResponsiveContainer
 } from "recharts";
 
@@ -40,23 +39,41 @@ const ClientStatisticChart = () => {
   };
 
   return (
-    <div className="w-full p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-lg font-semibold mb-4">Client Statistic</h2>
+    <div style={{width: "700px" , position: "absolute",top: "24.5%", right: "8%" , marginLeft: "30px"}} className="w-full p-6 bg-white rounded-lg shadow-md">
+
+      <svg style={{display: "none"}}>
+
+          <symbol id="america" width="16" height="16" viewBox="0 0 16 16" fill="" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <rect x="1" y="3" width="14" height="11" fill="url(#pattern0_13078_11815)"/>
+                <defs>
+                <pattern id="pattern0_13078_11815" patternContentUnits="objectBoundingBox" width="1" height="1">
+                <use xlink:href="#image0_13078_11815" transform="scale(0.0714286 0.0909091)"/>
+                </pattern>
+                <image id="image0_13078_11815" width="14" height="11" preserveAspectRatio="none" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAALCAIAAADA5ys1AAAAAXNSR0IArs4c6QAAAFNJREFUGBlj6OzcwMBQhpVczMCAjBiA6lavvsPAMAWT/I8Ezpw5wwA2D4s6oM4zDAxwBDR+SJkK9BqSR3EygcoYgV7TBPsOP3EdKE2aqfjNg8sCADSi4Rs7bpPpAAAAAElFTkSuQmCC"/>
+                </defs>
+          </symbol>
+
+      </svg>
       
-      <div className="flex gap-2 mb-4">
-        {["US", "UK", "France"].map((country, index) => (
-          <button
-            key={index}
-            className={`px-4 py-1 rounded-lg ${
-              selectedCountries.includes(country)
-                ? "bg-purple-500 text-white"
-                : "bg-gray-200 text-black"
-            }`}
-            onClick={() => toggleCountry(country)}
-          >
-            {country === "US" ? "🇺🇸 US" : country === "UK" ? "🇬🇧 UK" : "🇫🇷 France"}
-          </button>
-        ))}
+      <div className="title-container">
+        <h2 style={{fontFamily: "poppins-regular"}} className="text-lg font-semibold mb-4">Client Statistic</h2>
+        
+        <div style={{display: "flex"}} className="flex gap-8 mb-4">
+          {["US", "UK", "France"].map((country, index) => (
+            <div
+              style={{width: "58px" , height: "28px"}}
+              key={index}
+              className={`px-4 py-1 rounded-lg ${
+                selectedCountries.includes(country)
+                  ? "bg-purple-500 text-white"
+                  : "bg-gray-200 text-black"
+              }`}
+              onClick={() => toggleCountry(country)}
+            >
+              {country === "US" ? 'US' : country === "UK" ? "UK" : "France"}
+            </div>
+          ))}
+        </div>
       </div>
 
       <ResponsiveContainer width="100%" height={300}>
@@ -65,7 +82,6 @@ const ClientStatisticChart = () => {
           <XAxis dataKey="month" />
           <YAxis />
           <Tooltip />
-          <Legend />
 
           {selectedCountries.includes("US") && (
             <Line
